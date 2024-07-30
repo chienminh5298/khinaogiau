19266932cdf21eb0db64569392b45e999aefca01
Minh C Nguyen <chienminh5298@gmail.com>
2024-07-30 10:51:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://admin.moneymachine.minhnguyen.website:1234/landing/token/price/SOLUSDT`);
+                setData(response.data.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    const QSOL = 300;
+    const TSOL = 422;
+    return (
+        <div className="container">
+            {data ? (
+                <div>
+                    <p>Giá SOL: {data}</p>
+                    <p>
+                        Quâỵ có {QSOL} SOL tương đương: {parseInt(QSOL * data)}$
+                    </p>
+                    <p>
+                        Tiêu có {TSOL} SOL tương đương: {parseInt(TSOL * data)}$
+                    </p>
+                    <p>
+                        Tổng 2 thằng có: {QSOL + TSOL} tương đương: {parseInt((QSOL + TSOL) * data)}$
+                    </p>
+                </div>
+            ) : (
+                <p>Loading data...</p>
+            )}
+        </div>
+    );
+};
 
 export default App;
