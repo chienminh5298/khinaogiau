bbaf9d06681983861e3ded3a755a01e96ccaee1f
Minh C Nguyen <chienminh5298@gmail.com>
2024-08-01 14:16:28 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`https://admin.moneymachine.minhnguyen.website:1234/landing/token/price/SOLUSDT`);
+                const response = await axios.get(`https://adminmoneymachine.minhnguyen.website:1234/landing/token/price/SOLUSDT`);
                 setData(response.data.data);
             } catch (error) {
                 console.error("Error fetching data:", error);
