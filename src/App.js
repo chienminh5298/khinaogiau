2c20eff1a594217af6ca277cce1fb992553028f3
Minh Nguyen <chienminh5298@gmail.com>
2025-04-19 22:52:08 -0400

    update TXRP 2675

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ const App = () => {
 
     const QSOL = 226.24;
     const TSOL = 466.499;
-    const TXRP = 1716;
+    const TXRP = 2675;
     const XRPdautu = 7178;
     const di3SOL = 105.13;
     const chauSOL = 135.04;
